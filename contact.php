4edeadc516f09e541296f9be159a584a065cca3f
RonitrocketYT <ronitrocket98@gmail.com>
2019-10-19 17:05:01 -0400

    fixed form

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'ronittaleti05@gmail.com';
+$from = 'ronittaleti98@gmail.com';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'ronittaleti05@gmail.com';

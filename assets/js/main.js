adefe477cb3ef4c6300bfa10aa39bb04b866e7bd
ronitrocket <ronittaleti05@gmail.com>
2023-07-06 15:59:29 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,7 @@ NOTE: This is the custom jQuery file for the template
         var element = $(".element");
         $(function() {
             element.typed({
-                strings: ["VR/AR Enthusiast.", "AI/ML Enthusiast", "AI/ML Enthusiast", "Developer.", "Lifelong learner.", "Innovator at The Knowledge Society."],
+                strings: ["VR/AR Enthusiast.", "AI/ML Enthusiast.", "Developer.", "Lifelong learner.", "TKS Alumni."],
                 typeSpeed: 100,
                 loop: true,
             });
